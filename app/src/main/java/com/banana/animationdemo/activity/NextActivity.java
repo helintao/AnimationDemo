4c2474ad1d640c98a163d30dd6315b544f68a0cf
banana <979652261@qq.com>
2019-07-26 15:27:09 +0800

    '添加特殊场景下动画的使用（ListView）'

--- app/src/main/java/com/banana/animationdemo/activity/NextActivity.java
+++ app/src/main/java/com/banana/animationdemo/activity/NextActivity.java
@@ -3,14 +3,37 @@ package com.banana.animationdemo.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.banana.animationdemo.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NextActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    String [] strings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        listView=findViewById(R.id.next_lv);
+        strings=new String[15];
+        initData();
+        ArrayAdapter adapter=new ArrayAdapter<String>(
+                this,android.R.layout.simple_expandable_list_item_1,strings);
+        listView.setAdapter(adapter);
+
+    }
+
+    private void initData(){
+        for(int i=0;i<15;i++){
+            strings[i]="item:"+(i+1);
+        }
     }
 }
